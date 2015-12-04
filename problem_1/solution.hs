c998c7f9c27955c65a58021912fbafdfd0ab6d3d
Hawk Weisman <hi@hawkweisman.me>
2015-12-03 23:27:54 -0500

    Removed points

--- problem_1/solution.hs
+++ problem_1/solution.hs
@@ -1,8 +1,8 @@
 
 
 floorNum :: String -> Integer
-floorNum = let up_or_down '(' acc = acc + 1
-               up_or_down ')' acc = acc - 1
+floorNum = let up_or_down '(' = (+ 1) .
+               up_or_down ')' = (- 1) .
             in foldr up_or_down 0
 
 main :: IO ()
