953fc96883b358db6f03a8f501277b9ae393d01b
Hawk Weisman <hi@hawkweisman.me>
2015-12-03 23:20:53 -0500

    Solution for problem 1

---
+++ problem_1/solution.hs
@@ -0,0 +1,11 @@
+
+
+floorNum :: String -> Integer
+floorNum = let up_or_down '(' acc = acc + 1
+               up_or_down ')' acc = acc - 1
+            in foldr up_or_down 0
+
+main :: IO ()
+main = do
+    string <- getLine
+    print $ floorNum string
